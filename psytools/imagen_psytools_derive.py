eb764f35c52b6fe91f067f5331e5c6bf5a84f5b9
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2019-07-19 00:16:38 +0200

    Derivation before de-identification

--- psytools/imagen_psytools_derive.py
+++ psytools/imagen_psytools_derive.py
@@ -12,25 +12,25 @@ Attributes
 Input
 -----
 
-PSYTOOLS_FU3_PSC2_DIR : str
-    Location of PSC2-encoded FU3 files.
-PSYTOOLS_SB_PSC2_DIR : str
-    Location of PSC2-encoded Stratify files.
+PSYTOOLS_FU3_PSC1_DIR : str
+    Location of PSC1-encoded FU3 files.
+PSYTOOLS_SB_PSC1_DIR : str
+    Location of PSC1-encoded Stratify files.
 
 Output
 ------
 
-PSYTOOLS_FU3_PROCESSED_DIR : str
+PSYTOOLS_FU3_DERIVED_DIR : str
     Location of concatenated FU3 files.
-PSYTOOLS_SB_PROCESSED_DIR : str
+PSYTOOLS_SB_DERIVED_DIR : str
     Location of concatenated Stratify files.
 
 """
 
-PSYTOOLS_FU3_PSC2_DIR = '/neurospin/imagen/FU3/RAW/PSC2/psytools'
-PSYTOOLS_FU3_PROCESSED_DIR = '/neurospin/imagen/FU3/processed/psytools'
-PSYTOOLS_SB_PSC2_DIR = '/neurospin/imagen/STRATIFY/RAW/PSC2/psytools'
-PSYTOOLS_SB_PROCESSED_DIR = '/neurospin/imagen/STRATIFY/processed/psytools'
+PSYTOOLS_FU3_PSC1_DIR = '/neurospin/imagen/FU3/RAW/PSC1/psytools'
+PSYTOOLS_FU3_DERIVED_DIR = '/tmp/imagen/FU3/processed/psytools'
+PSYTOOLS_SB_PSC1_DIR = '/neurospin/imagen/STRATIFY/RAW/PSC1/psytools'
+PSYTOOLS_SB_DERIVED_DIR = '/tmp/imagen/STRATIFY/processed/psytools'
 
 
 import os
@@ -40,14 +40,14 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 
-def process(psc2_dir, processed_dir, input_template, output):
+def process(psc1_dir, derived_dir, input_template, output):
     """Concatenate LimeSurvey questionnaires from different centres.
 
     Parameters
     ----------
-    psc2_dir: str
-        Input directory with PSC2-encoded questionnaires.
-    processed_dir: str
+    psc1_dir: str
+        Input directory with PSC1-encoded questionnaires.
+    derived_dir: str
         Output directory with concatenated questionnaires.
     input_template: str
         Regex specifies file names associated to a questionnaire.
@@ -55,49 +55,49 @@ def process(psc2_dir, processed_dir, input_template, output):
         File name of concatenated questionnaire.
 
     """
-    psc2_paths = {}
-    for filename in os.listdir(psc2_dir):
+    psc1_paths = {}
+    for filename in os.listdir(psc1_dir):
         match = input_template.match(filename)
         if match:
             center = match.group(1)
-            psc2_paths[center] = os.path.join(psc2_dir, filename)
+            psc1_paths[center] = os.path.join(psc1_dir, filename)
 
     CENTER_ORDER = ('London', 'Nottingham', 'Dublin', 'Berlin',
                     'Hamburg', 'Mannheim', 'Paris', 'Dresden',
                     'Southampton', 'Aachen')
-    ordered_psc2_paths = []
+    ordered_psc1_paths = []
     for center in CENTER_ORDER:
-        if center in psc2_paths:
-            ordered_psc2_paths.append(psc2_paths[center])
-    for center in psc2_paths:
+        if center in psc1_paths:
+            ordered_psc1_paths.append(psc1_paths[center])
+    for center in psc1_paths:
         if center not in CENTER_ORDER:
-            ordered_psc2_paths.append(psc2_paths[center])
+            ordered_psc1_paths.append(psc1_paths[center])
 
-    processed_path = os.path.join(processed_dir, output)
-    with open(processed_path, 'w') as processed:
-        if ordered_psc2_paths:
-            with open(ordered_psc2_paths[0],'r') as psc2:
-                shutil.copyfileobj(psc2, processed)
-            for psc2_path in ordered_psc2_paths[1:]:
-                with open(psc2_path,'r') as psc2:
-                    psc2.readline()  # remove 1st line
-                    shutil.copyfileobj(psc2, processed)
+    derived_path = os.path.join(derived_dir, output)
+    with open(derived_path, 'w') as derived:
+        if ordered_psc1_paths:
+            with open(ordered_psc1_paths[0],'r') as psc1:
+                shutil.copyfileobj(psc1, derived)
+            for psc1_path in ordered_psc1_paths[1:]:
+                with open(psc1_path,'r') as psc1:
+                    psc1.readline()  # remove 1st line
+                    shutil.copyfileobj(psc1, derived)
 
 
 def main():
-    process(PSYTOOLS_FU3_PSC2_DIR, PSYTOOLS_FU3_PROCESSED_DIR,
+    process(PSYTOOLS_FU3_PSC1_DIR, PSYTOOLS_FU3_DERIVED_DIR,
             re.compile('Imagen_FUIII-Core1-([^\)]*).csv'),
             'Imagen_FUIII-Core1.csv')
-    process(PSYTOOLS_FU3_PSC2_DIR, PSYTOOLS_FU3_PROCESSED_DIR,
+    process(PSYTOOLS_FU3_PSC1_DIR, PSYTOOLS_FU3_DERIVED_DIR,
             re.compile('Imagen_FUIII-Core2-([^\)]*).csv'),
             'Imagen_FUIII-Core2.csv')
-    process(PSYTOOLS_FU3_PSC2_DIR, PSYTOOLS_FU3_PROCESSED_DIR,
+    process(PSYTOOLS_FU3_PSC1_DIR, PSYTOOLS_FU3_DERIVED_DIR,
             re.compile('Imagen_FUII-Parent-([^\)]*).csv'),
             'Imagen_FUII-Parent.csv')
-    process(PSYTOOLS_SB_PSC2_DIR, PSYTOOLS_SB_PROCESSED_DIR,
+    process(PSYTOOLS_SB_PSC1_DIR, PSYTOOLS_SB_DERIVED_DIR,
             re.compile('STRATIFY_Core1_\(([^\)]*)\).csv'),
             'STRATIFY_Core1.csv')
-    process(PSYTOOLS_SB_PSC2_DIR, PSYTOOLS_SB_PROCESSED_DIR,
+    process(PSYTOOLS_SB_PSC1_DIR, PSYTOOLS_SB_DERIVED_DIR,
             re.compile('STRATIFY_Core2_\(([^\)]*)\).csv'),
             'STRATIFY_Core2.csv')
 
